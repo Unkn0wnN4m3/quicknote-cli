f8fdb7cd2dd1b1db07f5227025e98c61f49b99dc
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-06-28 22:51:30 -0600

    feat: basic crud structure was added

---
+++ pynote/cli.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option(
+    "--title", default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"), help="Note title"
+)
+@click.option("--body", prompt="Note body", help="Body of your note")
+def new(title, body):
+    click.echo(f"{title}, {body}")
+
+
+@cli.command()
+def list():
+    click.echo("list notes")
+
+
+@cli.command()
+@click.option("--id", help="note id")
+@click.option("--title", help="New note title")
+@click.option("--body", prompt="New body", help="new body of your note")
+def edit(id, title, body):
+    click.echo(f"Updated note with id:{id}, title:{title}, body:{body}")
+
+
+@cli.command()
+@click.option("--id", help="note id")
+def delete(id):
+    click.echo(f"Deleted note with id:{id}")
