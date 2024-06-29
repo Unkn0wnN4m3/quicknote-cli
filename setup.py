e4753becd65761259d5a54be610ae085b2be29d1
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-06-28 23:33:38 -0600

    fix: sutup author fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pynote",
-    author="Unk0wnN4m3",
+    author="Unkn0wnN4m3",
     author_email="87839850+Unkn0wnN4m3@users.noreply.github.com",
     url="https://github.com/Unkn0wnN4m3/quicknote-cli",
     version="0.1.0",
